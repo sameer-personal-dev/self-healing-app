4f856504078e2b28072e33005cc55840a1e9aa12
Paramesh Anakapalli <panakapalli@salesforce.com>
2022-12-11 22:14:14 +0530

    handled onclick method to hide and show the chart

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,4 +1,12 @@
 import { LightningElement } from 'lwc';
 import LightningElementWithSLDS from '../../../lib/lightningElementWithSLDS.js';
 
-export default class App extends LightningElementWithSLDS {}
+export default class App extends LightningElementWithSLDS {
+    showSpeedDialDetails = false;
+    openSpeedDialDetails() {
+        this.showSpeedDialDetails = true;
+    }
+    showHome() {
+        this.showSpeedDialDetails = false;
+    }
+}
